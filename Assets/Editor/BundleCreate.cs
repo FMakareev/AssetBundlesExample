964a6d1155b4b418596b318668d032b91cdf7b8d
FMakareev <f.makareev@compaero.ru>
2018-08-21 22:03:23 +0300

    init

---
+++ Assets/Editor/BundleCreate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using System.Collections;
+
+public class BundleCreate
+{
+
+    [MenuItem("Simple Bundles/Build")]
+    static void BuildBundles()
+    {
+        // https://docs.unity3d.com/ScriptReference/EditorUtility.SaveFolderPanel.html
+        string path = EditorUtility.SaveFolderPanel("Save Bundle", "", "");
+        if (path.Length != 0)
+        {
+            // https://docs.unity3d.com/ScriptReference/BuildPipeline.html
+            BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        }
+    }
+}
---
+++ Assets/Scripts/LoadBundle.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadBundle : MonoBehaviour {
+
+    // Имя бандла
+    public string BundleName;
+    // Имя сцены
+    public string SceneName;
+
+    // Имя манифеста бандла
+    public string ManifestName;
+
+    // Манифест бандла
+    AssetBundleManifest Manifest;
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+        // загружаем манифест бандла
+        using (WWW www = new WWW("file://E:/UnityProjects/AssetBundlesExample/Data/" + ManifestName))
+        {
+            yield return www;
+
+            if (!string.IsNullOrEmpty(www.error))
+            {
+                Debug.Log(www.error);
+                yield break;
+            }
+            print(www.assetBundle);
+            // Получаем манифест из www 
+            //Manifest = www.assetBundle.LoadAsset("AssetBundleManifest") as AssetBundleManifest;
+            Manifest = www.assetBundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+
+            yield return null;
+
+            // Выгружаем из ОЗУ 
+            www.assetBundle.Unload(false);
+
+        }
+
+        // Получаем бандл, вторым аргументом передаем хеш из манифеста, сли хеш изменился то загрузится новый бандл если нет то загрузится и кеша
+        using (WWW www = WWW.LoadFromCacheOrDownload("file://E:/UnityProjects/AssetBundlesExample/Data/"+ BundleName, Manifest.GetAssetBundleHash(BundleName)))
+        {
+            yield return www;
+
+            if (!string.IsNullOrEmpty(www.error))
+            {
+                Debug.Log(www.error);
+                yield break;
+            }
+
+            SceneManager.LoadScene(SceneName, LoadSceneMode.Additive);
+            
+            yield return null;
+
+            www.assetBundle.Unload(true);
+
+        }
+
+
+	}
+	
+}
